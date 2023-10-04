79a9d6b884cf2358e75d7fcc0762853b8d890af4
raditya2010631170111 <2010631170111@student.unsika.ac.id>
2023-10-04 17:26:54 +0700

    1

--- app/Models/Namespace/Directory/Product.php
+++ app/Models/Namespace/Directory/Product.php
@@ -2,40 +2,22 @@
 
 namespace App\Models\Namespace\Directory;
 
-use CodeIgniter\Model;
+use App\Models\Namespace\Directory\Product as BaseProduct;
 
-class Product extends Model
+class ProductModel extends BaseProduct
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'products';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $useSoftDeletes = true;
+    protected $allowedFields = [
+        'name',
+        'stock',
+        'brand',
+        'buy_price',
+        'sale_price',
+        'comment'
+    ];
 
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
+    protected $useTimestamps = true;
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
