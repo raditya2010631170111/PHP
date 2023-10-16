19682eabcfe29a6bb54ef38053bf404a34c1175d
Raditya Aji Sasmoyo <115374523+raditya2010631170111@users.noreply.github.com>
2023-10-16 09:39:53 +0700

    Add files via upload

---
+++ app/Views/profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Profile</title>
+    <link rel="stylesheet" href="<?= base_url('css/style.css'); ?>">
+</head>
+<body>
+
+<div class="container">
+    <h1><?= $name ?></h1>
+    <p><?= $npm ?></p>
+    <p><?= $class ?></p>
+    <p><?= $phone ?></p>
+</div>
+
+</body>
+</html>
